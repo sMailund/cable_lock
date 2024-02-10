d0e4d2ae1bde311fc4667a2db5a28dd6efc0eada
Simen Mailund Svendsen <simen@erus.no>
2024-02-10 11:19:56 +0100

    password verification logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{arg, Command};
+use password_auth::VerifyError;
 
 fn cli() -> Command {
     Command::new("cable_lock")
@@ -12,6 +13,17 @@ fn cli() -> Command {
                 .arg(arg!(<SCOPE> "The requested scope"))
                 .arg_required_else_help(true),
         )
+}
+
+struct User {
+    username: String,
+    hash: String,
+    salt: String,
+}
+
+fn verify_password(user: User, password: &str) -> Result<(), VerifyError> {
+    let with_salt = format!("{}{}", password, user.salt);
+    password_auth::verify_password(with_salt, &*user.hash)
 }
 
 fn main() {
@@ -29,4 +41,31 @@ fn main() {
         }
 
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use password_auth::generate_hash;
+    use rand::distributions::{Alphanumeric, DistString};
+
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_salt() {
+        let salt = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+        let password = "password";
+        let salted = format!("{}{}", password, salt);
+        let hash = generate_hash(salted);
+
+        let user = User {
+            username: "test_user".to_string(),
+            hash,
+            salt,
+        };
+
+        let result = verify_password(user, password);
+        assert!(result.is_ok())
+    }
+
 }
