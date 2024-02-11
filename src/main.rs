adce171f9643b4de5e6934b3ff64768d4a6e4dee
Simen Mailund Svendsen <simen@erus.no>
2024-02-11 13:35:17 +0100

    print to stderr

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
                     println!("requested {}", scope);
                 }
                 Err(err) => {
-                    println!("{}", err);
+                    eprintln!("{}", err);
                     exit(1)
                 }
             }
