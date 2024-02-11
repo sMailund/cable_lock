774682d7c6770b22f37789a0364dfdaec6f463fc
Simen Mailund Svendsen <simen@erus.no>
2024-02-11 19:05:48 +0100

    use common location for migrations

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -80,17 +80,8 @@ mod tests {
 
     #[test]
     fn should_retrieve_stored_entry() {
-        // TODO: one common place to store migrations
-        let migrations = Migrations::new(vec![
-            M::up("create table authorization_code(auth_code TEXT PRIMARY KEY, subject TEXT NOT NULL, scopes TEXT NOT NULL);")
-                .down("drop table authorization_code;"),
-            M::up("create table user(username text NOT NULL UNIQUE, password_hash TEXT NOT NULL, password_salt TEXT NOT NULL);")
-                .down("drop table user;"),
-            // In the future, add more migrations here:
-            //M::up("ALTER TABLE friend ADD COLUMN email TEXT;"),
-        ]);
         let mut conn = Connection::open_in_memory().unwrap();
-        migrations.to_latest(&mut conn).unwrap();
+        apply_migrations(&mut conn);
 
         let scopes = vec!["scope"];
         let code = create_auth_code("user", scopes, &conn).unwrap();
@@ -104,17 +95,8 @@ mod tests {
 
     #[test]
     fn should_return_none_if_invalid_code() {
-        // TODO: one common place to store migrations
-        let migrations = Migrations::new(vec![
-            M::up("create table authorization_code(auth_code TEXT PRIMARY KEY, subject TEXT NOT NULL, scopes TEXT NOT NULL);")
-                .down("drop table authorization_code;"),
-            M::up("create table user(username text NOT NULL UNIQUE, password_hash TEXT NOT NULL, password_salt TEXT NOT NULL);")
-                .down("drop table user;"),
-            // In the future, add more migrations here:
-            //M::up("ALTER TABLE friend ADD COLUMN email TEXT;"),
-        ]);
         let mut conn = Connection::open_in_memory().unwrap();
-        migrations.to_latest(&mut conn).unwrap();
+        apply_migrations(&mut conn);
 
         let code = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
         let response =
