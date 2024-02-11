5a030e515244e4160a80f661eb07d40b7b0d4a12
Simen Mailund Svendsen <simen@erus.no>
2024-02-11 15:22:18 +0100

    add insert logic, todo make prettier

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,12 +1,26 @@
 use rand::distributions::{Alphanumeric, DistString};
 use rand::Rng;
-use rusqlite::Connection;
+use rusqlite::{Connection, params};
 
 const TOKEN_LENGTH: usize = 32;
 
-fn create_auth_code(username: &str, scopes: Vec<&str>, connection: Connection) -> String {
+fn create_auth_code(username: &str, scopes: Vec<&str>, connection: Connection) -> Result<String, String> {
     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
-    string
+
+   // here
+    let scopes_string = scopes.join(",");
+
+    let insert_result = connection.execute(
+        "INSERT INTO authorization_code (auth_code, subject, scopes) VALUES (?1, ?2, ?3)",
+        params![&string, username, scopes_string],
+    );
+
+    if (insert_result.is_err()) {
+        Err("failed to insert to db".to_string())
+    } else {
+        Ok(string)
+    }
+
 }
 
 // TODO return whole entry
@@ -37,7 +51,7 @@ mod tests {
         migrations.to_latest(&mut conn).unwrap();
 
         let scopes = vec!["scope"];
-        let code = create_auth_code("user", scopes, conn);
+        let code = create_auth_code("user", scopes, conn).unwrap();
         assert_ne!(0, code.len());
     }
 
@@ -56,7 +70,7 @@ mod tests {
         migrations.to_latest(&mut conn).unwrap();
 
         let scopes = vec!["scope"];
-        let code = create_auth_code("user", scopes, conn);
+        let code = create_auth_code("user", scopes, conn).unwrap();
         let response  = get_entry_by_auth_code(code.as_str());
         assert!(response.is_some());
         let string = response.unwrap();
